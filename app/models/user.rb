92bd47d8bbf24f13be2b21122221b6c725941d60
syunya-sr81 <gonsyun81@gmail.com>
2020-03-09 06:26:52 +0000

    pic commit

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    validates :name, presence: true,
+                     length: {maximum: 15}
+    validates :email, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
+    
+   has_secure_password
+    validates :password, length: {minimum: 8, maximum: 32}
+    VALID_PASWAD_REGEX = /\A[a-zA-Z0-9]+\z/
+    validates :email, presence: true, uniqueness: true, format: {with: VALID_PASWAD_REGEX}
+    
+    
+end
